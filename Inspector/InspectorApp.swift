b43d92b999358055371239313df1d90a934869fc
Stefan Urbanek <stefan@agentfarms.net>
2021-11-17 16:04:42 +0100

    Initial Commit

---
+++ Inspector/InspectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  InspectorApp.swift
+//  Inspector
+//
+//  Created by Stefan Urbanek on 2021/11/17.
+//
+
+import SwiftUI
+
+@main
+struct InspectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
